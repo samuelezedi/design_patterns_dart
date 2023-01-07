1e425e60c542bf3c8fb0dbd80fd1f94f8528cf2e
Samuel Ezedi <sammy.ezedi@gmail.com>
2023-01-07 20:00:32 +0100

    Update: I have included singleton design pattern code

--- bin/creational/singleton.dart
+++ bin/creational/singleton.dart
@@ -13,3 +13,8 @@ class Person {
 
   String name = "Samuel";
 }
+
+
+///
+///In this example, the Singleton class has a private constructor and a static field named _instance that holds the single instance of the class. The class also has a factory constructor that returns the _instance field. This ensures that there is only one instance of the class, and that the instance is created when it is first accessed.
+///The main function creates two Singleton objects using the factory constructor. Since the factory constructor always returns the same instance, the two objects are equal and have the same value.
